49b1515cbf0f5efff7998ebdd32a9817479b8e4d
Craig S. Cottingham <craig@cottingham.net>
2018-12-16 01:34:12 -0600

    Add (single) waypoint parser

--- lib/ksp_maps/parsers/waypoints_parser.ex
+++ lib/ksp_maps/parsers/waypoints_parser.ex
@@ -3,23 +3,86 @@ defmodule KSPMaps.WaypointsParser do
 
   require Logger
 
-  def parse_comment({:ok, data, stream}), do: parse_comment({:ok, data, "", stream})
-  def parse_comment({:ok, _, "", _} = state), do: reload_buffer(state) |> parse_comment
-  def parse_comment({:ok, data, buffer, stream}) do
+  import NimbleParsec
+
+  def parse({:ok, data, stream}), do: parse({:ok, data, "", stream})
+  def parse({:ok, _, "", _} = state), do: reload_buffer(state) |> parse
+  def parse({:ok, data, buffer, stream}) do
     case buffer do
       "//" <> remaining ->
         {:ok, data, remaining, stream}
         |> strip_comment
-      _ ->
-        {:not_found, data, buffer, stream}
+      "WAYPOINT" <> remaining ->
+        {:ok, data, remaining, stream}
+        |> parse_waypoint
     end
   end
 
-  defp strip_comment({:ok, data, <<10, remaining::binary>>, stream}), do: {:ok, data, remaining, stream}
-  defp strip_comment({:ok, data, <<13, 10, remaining::binary>>, stream}), do: {:ok, data, remaining, stream}
-  defp strip_comment({:ok, data, <<13, remaining::binary>>, stream}), do: {:ok, data, remaining, stream}
-  defp strip_comment({:ok, _, "", _} = state), do: reload_buffer(state) |> strip_comment
-  defp strip_comment({:ok, data, <<_::bytes-size(1), remaining::binary>>, stream}), do: strip_comment({:ok, data, remaining, stream})
+  def strip_comment({:ok, data, <<10, remaining::binary>>, stream}), do: {:ok, data, remaining, stream}
+  def strip_comment({:ok, data, <<13, 10, remaining::binary>>, stream}), do: {:ok, data, remaining, stream}
+  def strip_comment({:ok, data, <<13, remaining::binary>>, stream}), do: {:ok, data, remaining, stream}
+  def strip_comment({:ok, _, "", _} = state), do: reload_buffer(state) |> strip_comment
+  def strip_comment({:ok, data, <<_::bytes-size(1), remaining::binary>>, stream}), do: strip_comment({:ok, data, remaining, stream})
+
+
+  # nul, \t, \n, \f, \r, space respectively
+  whitespace_values = [0, 9, 10, 12, 13, 32]
+  whitespace_char = utf8_char(whitespace_values)
+
+  # \n, \r, } respectively
+  value_terminator_values = [10, 13, ?}]
+  value_terminator_char = utf8_char(value_terminator_values)
+
+  name = ignore(repeat(whitespace_char))
+         |> repeat(lookahead_not(utf8_char([?=])) |> utf8_char([]))
+         |> reduce({List, :to_string, []})
+         |> map({String, :trim, []})
+         |> unwrap_and_tag(:name)
+
+  value = repeat(lookahead_not(value_terminator_char) |> utf8_char([]))
+          |> reduce({List, :to_string, []})
+          |> map({String, :trim, []})
+          |> unwrap_and_tag(:value)
+
+  # [name: "Name", value: "value"]
+  def resolve_pair([{:name, name}, {:value, value}]), do: [{:name, name}, {:value, value}]
+
+  defcombinatorp :pair,
+    name
+    |> ignore(repeat(whitespace_char))
+    |> ignore(utf8_char([?=]))
+    |> concat(value)
+    |> reduce(:resolve_pair)
+
+  # {
+  # 	name = The Great Desert
+  # 	celestialName = Kerbin
+  # 	latitude = 2.4902494069446099
+  # 	longitude = 218.604135349567
+  # 	navigationId = 87b0c496-500d-426f-b9b3-dbf75c2320e4
+  # 	icon = eva
+  # 	altitude = 3.4106051316484809E-13
+  # 	index = 0
+  # 	seed = 173
+  # }
+  defparsec :waypoint,
+    ignore(repeat(whitespace_char))
+    |> ignore(utf8_char([?{]))
+    |> repeat(lookahead_not(utf8_char([?}])) |> parsec(:pair))
+    |> ignore(utf8_char([?}]))
+    |> tag(:waypoint)
+    |> ignore(repeat(whitespace_char))
+
+  def parse_waypoint({:ok, _, "", _} = state), do: reload_buffer(state) |> parse_waypoint
+  def parse_waypoint({:ok, data, buffer, stream}) do
+    # Logger.warn fn -> inspect(waypoint(buffer)) end
+    # {:ok, [waypoint: [[name: "name ", value: " foo "]]], "remainder", %{}, {1, 0}, 15}
+    case waypoint(buffer) do
+      {:ok, [waypoint: [[name: name, value: value]]], remainder, _, _, _} ->
+        {:ok, [data, %{name => value}], remainder, stream}
+    end
+  end
+
 
   defp reload_buffer({_, data, buffer, stream}) do
     case IO.binread(stream, 1024) do
--- spec/ksp_maps/parsers/waypoints_parser_spec.exs
+++ spec/ksp_maps/parsers/waypoints_parser_spec.exs
@@ -5,14 +5,36 @@ defmodule KSPMaps.WaypointsParser.Spec do
 
   doctest KSPMaps.WaypointsParser
 
-  example_group "parsing comments" do
+  example_group "parsing a waypoint" do
     before do
       {:ok, stream_pid} = StringIO.open(input())
       {:shared, stream: stream_pid}
     end
 
     let :initial_data, do: []
-    let :parsed, do: KSPMaps.WaypointsParser.parse_comment({:ok, initial_data(), shared.stream})
+    let :parsed, do: KSPMaps.WaypointsParser.parse_waypoint({:ok, initial_data(), "", shared.stream})
+    let :status, do: parsed() |> elem(0)
+    let :data, do: parsed() |> elem(1) |> List.flatten
+    let :remainder, do: parsed() |> elem(2)
+
+    context "given simple input" do
+      let :input, do: "{ name = foo } remainder"
+      it do
+        expect (status()) |> to(eq(:ok))
+        expect (data()) |> to(eq([%{"name" => "foo"}]))
+        expect (remainder()) |> to(eq("remainder"))
+      end
+    end
+  end
+
+  example_group "stripping a comment" do
+    before do
+      {:ok, stream_pid} = StringIO.open(input())
+      {:shared, stream: stream_pid}
+    end
+
+    let :initial_data, do: []
+    let :parsed, do: KSPMaps.WaypointsParser.strip_comment({:ok, initial_data(), "", shared.stream})
     let :status, do: parsed() |> elem(0)
     let :data, do: parsed() |> elem(1)
     let :remainder, do: parsed() |> elem(2)
