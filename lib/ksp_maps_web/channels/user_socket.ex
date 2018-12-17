facf1a336d4fd8fb014447b14801f4ee407a2f51
Craig S. Cottingham <craig@cottingham.net>
2018-12-16 23:35:57 -0600

    Add data channel to socket

--- lib/ksp_maps_web/channels/user_socket.ex
+++ lib/ksp_maps_web/channels/user_socket.ex
@@ -3,6 +3,7 @@ defmodule KSPMapsWeb.UserSocket do
 
   ## Channels
   # channel "room:*", KSPMapsWeb.RoomChannel
+  channel "data:*", KSPMapsWeb.DataChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
