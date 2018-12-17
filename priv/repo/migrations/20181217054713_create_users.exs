c620b9d53cd0a938859c720c5162feb2bc5fe5bf
Craig S. Cottingham <craig@cottingham.net>
2018-12-17 00:06:26 -0600

    Add migration for pow users

---
+++ priv/repo/migrations/20181217054713_create_users.exs
@@ -0,0 +1,16 @@
+defmodule KSPMaps.Repo.Migrations.CreateUsers do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string, null: false
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
