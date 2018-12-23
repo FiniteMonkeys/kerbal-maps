0b7d22fd019b7424f836ddafcf0420961d3bc74a
Craig S. Cottingham <craig@cottingham.net>
2018-12-22 23:39:34 -0600

    Fix channel authentication

--- lib/kerbal_maps_web/channels/user_socket.ex
+++ lib/kerbal_maps_web/channels/user_socket.ex
@@ -20,19 +20,19 @@ defmodule KerbalMapsWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    salt = Application.get_env(:kerbal_maps, KerbalMapsWeb.Endpoint)
+           |> Keyword.get(:secret_key_base)
+
+    # max_age: 1209600 is equivalent to two weeks in seconds
+    case Phoenix.Token.verify(socket, salt, token, max_age: 1_209_600) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     KerbalMapsWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
-  #
   # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "user_socket:#{socket.assigns.user_id}"
 end
