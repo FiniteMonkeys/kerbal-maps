3a519e19089adcbc2421c8e4d5486ac6f4517830
Craig S. Cottingham <craig@cottingham.net>
2019-01-06 14:34:23 -0600

    Add repo details to config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -69,6 +69,14 @@ config :logger, level: :info
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
 
+# Configure your database
+config :kerbal_maps, KerbalMaps.Repo,
+  username: "postgres",
+  password: "",
+  database: "kerbal_maps",
+  hostname: "localhost",
+  pool_size: 10
+
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
 import_config "prod.secret.exs"
