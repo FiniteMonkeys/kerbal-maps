8e75d1ec43b22ee30e6b25a2183424a961a7e557
Craig S. Cottingham <craig@cottingham.net>
2019-01-12 23:29:27 -0600

    Add gcp.tf
    Contains only TF configuration for database instance and database

---
+++ gcp.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  credentials = "${file("credentials.json")}"
+  project     = "kerbal-maps"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_sql_database_instance" "master" {
+  region           = "us-central1"
+  # name             = "master-instance"
+  database_version = "POSTGRES_9_6"
+
+  settings {
+    tier      = "db-f1-micro"
+    # disk_size = "1"
+  }
+}
+
+resource "google_sql_database" "kerbal_maps" {
+  name      = "kerbal_maps"
+  instance  = "${google_sql_database_instance.master.name}"
+
+  charset   = "UTF8"
+  collation = "en_US.UTF8"
+}
