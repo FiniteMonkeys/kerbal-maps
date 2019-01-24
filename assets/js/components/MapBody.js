0b009ccb733d73f3065379ea9502276e03461d82
Craig S. Cottingham <craig@cottingham.net>
2019-01-23 20:51:13 -0600

    Reformat body and style pane

--- assets/js/components/MapBody.js
+++ assets/js/components/MapBody.js
@@ -13,11 +13,12 @@ class MapBody extends React.Component {
   render () {
     const selectedValue = this.props.selectedValue
     return (
-      <fieldset>
-        <label htmlFor="select-map-body">Body:</label>
+      <div className="form-group">
+        <label htmlFor="select-map-body">Body</label>
         <select id="select-map-body"
                 name="select-map-body"
                 value={selectedValue}
+                className="form-control"
                 onChange={this.changeValue}>
           <option value="moho">Moho</option>
           <option value="eve">Eve</option>
@@ -36,7 +37,7 @@ class MapBody extends React.Component {
           <option value="pol">Pol</option>
           <option value="eeloo">Eeloo</option>
         </select>
-      </fieldset>
+      </div>
     )
   }
 }
