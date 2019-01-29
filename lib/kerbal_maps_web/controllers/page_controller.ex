871667d7651dd0420e79fc0829236c9ef51ea532
Craig S. Cottingham <craig@cottingham.net>
2019-01-28 22:12:18 -0600

    Handle body and loc query params

--- lib/kerbal_maps_web/controllers/page_controller.ex
+++ lib/kerbal_maps_web/controllers/page_controller.ex
@@ -5,14 +5,18 @@ defmodule KerbalMapsWeb.PageController do
 
   use KerbalMapsWeb, :controller
 
+  require Logger
+
   alias Pow.Plug
 
-  def index(conn, _params) do
+  def index(conn, params) do
     login_changeset = Plug.change_user(conn)
 
     render(conn, "index.html",
       login_changeset: login_changeset,
-      login_action: Routes.pow_session_path(conn, :create)
+      login_action: Routes.pow_session_path(conn, :create),
+      body_from_query: get_body_from_params(params),
+      loc_from_query: get_loc_from_params(params)
     )
   end
 
@@ -21,4 +25,14 @@ defmodule KerbalMapsWeb.PageController do
     |> Enum.find(fn t -> elem(t, 0) == :kerbal_maps end)
     |> elem(2)
   end
+
+  # %{"body" => "Kerbin", "loc" => "12.34,-56.78"}
+  def get_body_from_params(%{"body" => value}), do: value |> String.downcase
+  def get_body_from_params(_), do: nil
+
+  def get_loc_from_params(%{"loc" => value}) do
+    KerbalMaps.CoordinateParser.parse(value)
+    |> Enum.join(",")
+  end
+  def get_loc_from_params(_), do: nil
 end
--- spec/kerbal_maps_web/controllers/page_controller_spec.exs
+++ spec/kerbal_maps_web/controllers/page_controller_spec.exs
@@ -8,10 +8,20 @@ defmodule KerbalMapsWeb.PageController.Spec do
   example_group "GET /" do
     context "without a query string" do
       let :request, do: build_conn() |> get("/")
+      let :response, do: html_response(request(), 200)
 
-      it "returns all package names" do
-        expect(html_response(request(), 200)) |> to(match("<title>Kerbal Maps</title>"))
-      end
+      it "returns a valid web page", do: expect(response()) |> to(match("<title>Kerbal Maps</title>"))
+      it "does not set bodyFromQuery", do: expect(response()) |> to_not(match("<script>window.bodyFromQuery"))
+      it "does not set locFromQuery", do: expect(response()) |> to_not(match("<script>window.locFromQuery"))
+    end
+
+    context "with a query string" do
+      let :request, do: build_conn() |> get("/?body=Kerbin&loc=12.34,-56.78")
+      let :response, do: html_response(request(), 200)
+
+      it "returns a valid web page", do: expect(response()) |> to(match("<title>Kerbal Maps</title>"))
+      it "sets bodyFromQuery", do: expect(response()) |> to(match("<script>window.bodyFromQuery"))
+      it "sets locFromQuery", do: expect(response()) |> to(match("<script>window.locFromQuery"))
     end
   end
 end
