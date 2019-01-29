64263e236c7656fd096f367783fa631c9c3a2de4
Craig S. Cottingham <craig@cottingham.net>
2019-01-28 21:48:41 -0600

    Remove unneeded JS file

--- assets/js/credits.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Credits from "./components/Credits.js"
-
-export function renderCredits() {
-  ReactDOM.render(<Credits />, document.getElementById("credits"))
-}
