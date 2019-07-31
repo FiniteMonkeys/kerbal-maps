282d19a041276bdb2ee476089346e5533cce4e65
Craig S. Cottingham <craig@cottingham.net>
2019-07-30 21:53:33 -0500

    Comment out Distillery config overlay

--- rel/config.exs
+++ rel/config.exs
@@ -39,10 +39,10 @@ release :kerbal_maps do
   set applications: [
     :runtime_tools
   ]
-  set config_providers: [
-    {Mix.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/etc/config.exs"]}
-  ]
-  set overlays: [
-    {:copy, "rel/config/config.exs", "etc/config.exs"}
-  ]
+  # set config_providers: [
+  #   {Mix.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/etc/config.exs"]}
+  # ]
+  # set overlays: [
+  #   {:copy, "rel/config/config.exs", "etc/config.exs"}
+  # ]
 end
